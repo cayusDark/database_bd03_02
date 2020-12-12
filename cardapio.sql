572b388d1ee62fe92ebbe7311f45c843478712a3
Caio_Freire <caiooliveirafreire@gmail.com>
2020-12-12 16:28:08 -0400

    organização

--- cardapio.sql
+++
@@ -1,4 +0,0 @@
-select c.nome, p.nome, pr.tamanho, pr.preco from cardapios c
-inner join consta co on c.id=co.fk_cardapios_id
-inner join pizzas p on co.fk_pizzas_id=p.id
-inner join precos pr on p.id=pr.fk_pizzas_id;
